2dd49f32b8480d0fe12057e8105e72cb570a28d9
bjoernaf <bjfo5755@student.uu.se>
2015-01-19 12:17:33 +0100

    Added search_taste and search_unclear

--- search.R
+++ search.R
@@ -48,6 +48,41 @@ search_wine_artnr <- function(artnr) {
     return(all_wines[all_wines$Varnummer == artnr, ])
 }
 
+# search_wine_name(name)
+# BRIEF: Returns all wines that contains the string given as argument.
+# ARGUMENTS:
+# name = The string to search for in wine names.
+# RETURNS: All wines whose name contains the given string.
+# PRE:   Requires the all_wine frame to be built by wine_classify.
+search_wine_name <- function(name) {
+
+  return(all_wines[
+	    which(grepl(name, all_wines$Namn, F, F, T) | 
+	    grepl(name, all_wines$Namn2, F, F, T)),])
+
+}
+
+# search_wine_predscore_price(minscore, maxscore, minprice, maxprice)
+# BRIEF: Returns all wines that have a score between minscore and maxscore, with
+#         a price between minprice and maxscore. Both intervals are inclusive.
+# ARGUMENTS:
+# minscore = The lowest score to allow.
+# maxscore = The highest score to allow.
+# minprice = The lowest price to allow.
+# maxprice = The highest price to allow.
+# RETURNS: All wines within the given score and price range.
+# PRE: Requires the all_wines frame to be built by classify.
+search_wine_predscore_price <- function(minscore, maxscore, minprice, maxprice) {
+
+  return(all_wines[which(
+	    all_wines$PredictedScore >= minscore &
+	    all_wines$PredictedScore <= maxscore &
+	    (all_wines$PrisPerLiter * 0.75) >= minprice &
+	    (all_wines$PrisPerLiter * 0.75) <= maxprice)
+	,])
+
+}
+
 # search_topN(N, show_only_new)
 # BRIEF: Gives the top N wines based on rating. If show_only_new is not given
 #         (or is given as F), this includes wines which already have a score, if
@@ -61,9 +96,9 @@ search_wine_artnr <- function(artnr) {
 search_topN <- function(N, show_only_new = F) {
 
     if(show_only_new) {
-	result = head(all_wines[!is.finite(all_wines$GivenScore),], N)
+	result <- head(all_wines[!is.finite(all_wines$GivenScore),], N)
     } else {
-	result = head(all_wines, N)
+	result <- head(all_wines, N)
     }
 
     # Return the result
@@ -77,8 +112,8 @@ search_topN <- function(N, show_only_new = F) {
 #       all_wines frame.
 search_grapes <- function() {
   
-  grapes = make_score_RCGY(all_wines)
-  grapes = grapes[which(grapes$type == "Grape"),]
+  grapes <- make_score_RCGY(all_wines)
+  grapes <- grapes[which(grapes$type == "Grape"),]
   return(grapes[order(grapes$score, decreasing = TRUE), c("name", "score")])
 
 }
@@ -89,7 +124,64 @@ search_grapes <- function() {
 #       the all_wines frame.
 search_taste <- function() {
 
-  taste = make_score_taste(all_wines)
+  taste <- make_score_taste(all_wines)
   return(taste[order(taste$score, decreasing = TRUE), c("word", "score")])
+
+}
+
+# search_wine_unclear_score()
+# BRIEF: Returns the wines in order of how much the different score classes
+#         differ. This might indicate that there is insufficient data for that
+#         wine, and that it will help increase the overall predictions if it is
+#         given a score.
+search_wine_unclear_score <- function() {
+
+  my_wine <- all_wines
+
+  for(i in 1:length(my_wine$Varnummer)) {
+
+    myvec = c(my_wine$RCGY_predicted[i], 
+	      my_wine$Taste_predicted[i], 
+	      my_wine$bar_predicted[i])
+    
+    mymin <- min(myvec, na.rm = T)
+
+    mymax <- max(myvec, na.rm = T)
+
+    if(is.finite(mymin) & is.finite(mymax)) {
+      my_wine$PredScoreDiff[i] = (mymax - mymin)
+    } else {
+      my_wine$PredScoreDiff[i] = 0
+    }
+
+    if(!is.na(my_wine$GivenScore[i])) {
+      my_wine$PredScoreTest[i] = "X"
+    } else {
+      my_wine$PredScoreTest[i] = ""
+    }
+
+  }
+
+  return(my_wine[order(my_wine$PredScoreDiff, decreasing = T),])
+
+}
+
+# search_taste_terms(tvec)
+# BRIEF: Returns all wines which contains the specified taste strings.
+# ARGUMENTS:
+# tvec = A vector of search terms.
+search_taste_terms <- function(tvec) {
+
+  search_string <- "^"
+
+  for(i in 1:length(tvec)) {
+
+    search_string <- paste(search_string, "(?=.*\\b", tvec[i], "\\b)", sep="")
+
+  }
+
+  search_string <- paste(search_string, ".*$", sep="")
+
+  return(all_wines[which(grepl(search_string, all_wines$smak, F, T)),])
 
 }
