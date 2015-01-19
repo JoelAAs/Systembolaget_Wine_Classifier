492dc08e5738556a1165e770abe9637c537ad13e
bjoernaf <bjfo5755@student.uu.se>
2015-01-19 15:10:13 +0100

    Updated table presentation

--- present.R
+++ present.R
@@ -3,7 +3,9 @@
 # ARGUMENTS:
 # wine_table = The data frame containing the wines to list.
 present_wine_tbl <- function(wine_table) {
-  
+ 
+  message("")
+
   for(i in 1:length(wine_table$Varnummer)) {
    
     message("")
@@ -13,10 +15,10 @@ present_wine_tbl <- function(wine_table) {
       full_name = paste(full_name, "-", wine_table$Namn2[i])
     }
 
-    message("+---- ", full_name, " -------------")
+    message("+----[ ", full_name, " ]-------------")
     message("| Nr: ", wine_table$Varnummer[i], 
-	    "  Predicted score: ", round(wine_table$PredictedScore[i]),
-	    "  Given score: ", wine_table$GivenScore[i])
+	    "   Predicted score: ", round(wine_table$PredictedScore[i]),
+	    "   Given score: ", wine_table$GivenScore[i])
     message("| ")
     
     smak_str = wine_table$smak[i]
@@ -25,10 +27,13 @@ present_wine_tbl <- function(wine_table) {
     message("| Taste keywords: ")
     message("| ", smak_str)
     message("|")
-    message("| Grape: ", wine_table$RavarorBeskrivning[i])
+    message("| Grape: ", wine_table$RavarorBeskrivning[i],
+	    "   Year: ", wine_table$Argang[i])
 
     message("|")
-    message("| Price: ", round(wine_table$PrisPerLiter[i] * 0.75), " kr")
+    message("| Price: ", round(wine_table$PrisPerLiter[i] * 0.75), " kr",
+	    "   Region: ", wine_table$Ursprung[i], 
+	    ", ",wine_table$Ursprunglandnamn[i])
 
   }
 
