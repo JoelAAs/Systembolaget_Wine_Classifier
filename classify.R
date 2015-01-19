c9b48ed2faa7a48218929d3eb2fa6dfce15eb576
bjoernaf <bjfo5755@student.uu.se>
2015-01-19 19:25:51 +0100

    Fixed Grape bug in classify

--- classify.R
+++ classify.R
@@ -54,9 +54,9 @@ get_score_per_word <- function(word_list, score_list){
 # current_score = The dictionary to use.
 # RETURNS: The score for the term if found, NA otherwise.
 get_score_sub <- function(in_str, current_score){
-  
+
   if(in_str %in% current_score$name){
-    score <- current_score$score[grepl(in_str,current_score$name, F, F, T)]
+    score <- current_score$score[which(in_str == current_score$name)]
   }else{
     score <- NA
   }
