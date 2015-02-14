9427d965637f7d50e0baee3c4e71dc0cafb47f70
JoelAA <joel.as.0808@gmail.com>
2015-02-14 01:32:21 +0100

    filter func

--- out2php.R
+++ out2php.R
@@ -5,17 +5,23 @@ options(echo=TRUE) # if you want see commands in output file
 args <- commandArgs(trailingOnly = TRUE)
 
 start = items*as.numeric(args[1]) + 1
-stop  = items*as.numeric(args[1]) + items 
+stop  = items*as.numeric(args[1]) + items
 
 if(start > length(myWine[,1])){
   print(ERR)
 } else if(length(args) == 1) {
-    print(myWine[c(start:stop),])
-} else if(length(args)%%2 != 1) {
-  print(ERR)
+  if(length(args)%%2 != 1)   {
+    print(ERR)
+  } else if(start > length(myWine[,1])){
+    print(ERR)
+  } else if (length(myWine[,1]) > stop) {
+  print(myWine[c(start:stop),])
+  } else {
+  print(myWine[c(start:length(myWine[,1])),])
+  }
 } else {
-  for (i in seq(2,length(args), by=2)) {
-    myWine <- myWine[which(myWine[[args[i]]] == args[i+1]),]
+  for (i in seq(2, length(args), by = 3)) {
+    myWine<-filter_wine(args[i],args[i+1],args[i+2])
   }
   if(start > length(myWine[,1])){
     print(ERR)
@@ -25,3 +31,5 @@ if(start > length(myWine[,1])){
     print(myWine[c(start:length(myWine[,1])),])
   }
 }
+
+myWine <- myWine[which(myWine[[args[i]]] == args[i+1]),]
